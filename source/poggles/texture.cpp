e71aaa6de0b217f13d48a78a618d8d63b99ced93
Jeremy Hart <jeremy@vividtheory.com>
2023-03-16 11:22:12 -0600

    Texture wrapping

--- source/poggles/texture.cpp
+++ source/poggles/texture.cpp
@@ -50,8 +50,8 @@ void poggles::texture::load(GLenum target,
 
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
   glTexImage2D(target,
                level,
