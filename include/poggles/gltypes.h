7c7d4820400bd12006bdd7600c55153fe8db9500
Lakin Wecker <lakin@wecker.ca>
2023-06-08 09:58:12 -0600

    Fixing lints

--- include/poggles/gltypes.h
+++ include/poggles/gltypes.h
@@ -20,8 +20,8 @@ protected:
   T m_id;
   // Protected constructor so it can't be created directly (Should only be
   // created using handle class)
-  explicit gltype(T v)
-      : m_id(v)
+  explicit gltype(T in_id)
+      : m_id(in_id)
   {
   }
 
@@ -34,8 +34,11 @@ public:
   }
 
   gltype(gltype<T> const& other) = default;
+  gltype(gltype<T>&&) noexcept = default;
+  auto operator=(const gltype<T>&) -> gltype<T>& = default;
+  auto operator=(gltype<T>&&) noexcept -> gltype<T>& = default;
 
-  T id() const { return m_id; }
+  auto id() const -> T { return m_id; }
   operator T() const { return m_id; }
   operator bool() const { return m_id != 0; }
 };
