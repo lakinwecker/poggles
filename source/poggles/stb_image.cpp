95c5e3ee8608d6e6d687bfb1358b65b3568adccc
Lakin Wecker <lakin@wecker.ca>
2023-06-11 19:59:02 -0600

    Apparently this isn't needed?

--- source/poggles/stb_image.cpp
+++ source/poggles/stb_image.cpp
@@ -2,6 +2,6 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wold-style-cast"
 
-#define STB_IMAGE_IMPLEMENTATION
+// #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 // NOLINTEND
