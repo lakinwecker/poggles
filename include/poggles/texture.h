9a153c95ad3396a171cdfcf9ff5cd986326231a1
JeremyAdamHart <jeremyadamhart@gmail.com>
2023-06-28 11:39:39 -0600

    Modification for texture PR
    * fix: Fixing lints (#28)
    
    Co-authored-by: Lakin Wecker <lakin@wecker.ca>
    
    * v0.3.3 - fix: Fixing lints (#28)
    
    Co-authored-by: Lakin Wecker <lakin@wecker.ca>
    
    * Rework Texture Interface
    
    Changes the texture interface use std::variant<> instead of individual load functions. This has the following benfits:
    - Reduces code duplication. No need to specify GL_TEXTURE_2D (for example) in the constructor and the load function.
    - Clearer user API. Users don't need to worry about which load function to call, they just pass in their data and that's it.
    - Easy upgrade path. In the future we can add different texture types without adding another load function.
    
    * Texture loading rework
    
    * Use same syntax for input parameters that Lakin used
    
    * Use target in glTexParameteri
    
    * Move target assignment earlier
    
    ---------
    
    Co-authored-by: Lakin Wecker <lakin.wecker@gmail.com>
    Co-authored-by: Lakin Wecker <lakin@wecker.ca>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Erik Biederstadt <biederstadterik@gmail.com>

--- include/poggles/texture.h
+++ include/poggles/texture.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <filesystem>
 #include <optional>
 #include <string>
 #include <variant>
@@ -11,59 +12,33 @@
 
 namespace poggles
 {
-struct TextureFromFile
-{
-  std::string filename;
-  GLint level;
-  std::optional<GLenum> target;
-};
-
-struct TextureFromData
-{
-  TextureFromData(std::variant<float*, uint8_t*> _data,
-                  int _width,
-                  int _height,
-                  int _channels,
-                  GLint _level,
-                  GLenum _target);
-  TextureFromData(std::variant<float*, uint8_t*> _data,
-                  int _width,
-                  int _height,
-                  int _channels);
-
-  std::variant<float*, uint8_t*> data;
-  int width;
-  int height;
-  int channels;
-  GLint level;
-  std::optional<GLenum> target;
-};
-
-using TextureType = std::variant<TextureFromFile, TextureFromData>;
-
 class POGGLES_EXPORT texture
 {
 public:
-  explicit texture(GLenum target);
+  explicit texture(GLenum in_target);
 
-  explicit operator GLuint() const
-  {
-    return static_cast<GLuint>(m_texture_handle.value());
-  }
+  explicit operator GLuint() const;
 
   static void activate(GLenum texture_num) { glActiveTexture(texture_num); }
 
-  void bind(GLenum texture_num = GL_TEXTURE0) const
-  {
-    activate(texture_num);
-    glBindTexture(m_original_target,
-                  static_cast<GLuint>(m_texture_handle.value()));
-  }
+  void bind(GLenum texture_num = GL_TEXTURE0) const;
 
-  void load(TextureType type);
+  GLenum target;
 
 private:
-  GLenum m_original_target;
   texture_handle m_texture_handle;
 };
+
+auto loadFromFile(texture& tex,
+                  std::filesystem::path const& filepath,
+                  std::optional<GLenum> target = {},
+                  GLint level = 0) -> bool;
+
+auto loadFromData(texture& tex,
+                  std::variant<float*, uint8_t*> data,
+                  int width,
+                  int height,
+                  int channels,
+                  std::optional<GLenum> target = {},
+                  GLint level = 0) -> bool;
 }  // namespace poggles
--- source/poggles/texture.cpp
+++ source/poggles/texture.cpp
@@ -4,33 +4,6 @@
 
 #include "stb_image.h"
 
-poggles::TextureFromData::TextureFromData(std::variant<float*, uint8_t*> _data,
-                                          int _width,
-                                          int _height,
-                                          int _channels)
-    : data(_data)
-    , width(_width)
-    , height(_height)
-    , channels(_channels)
-    , level(0)
-{
-}
-
-poggles::TextureFromData::TextureFromData(std::variant<float*, uint8_t*> _data,
-                                          int _width,
-                                          int _height,
-                                          int _channels,
-                                          GLint _level,
-                                          GLenum _target)
-    : data(_data)
-    , width(_width)
-    , height(_height)
-    , channels(_channels)
-    , level(_level)
-    , target(_target)
-{
-}
-
 // https://www.cppstories.com/2018/09/visit-variants/
 template<class... Ts>
 struct overload : Ts...
@@ -40,86 +13,100 @@ struct overload : Ts...
 template<class... Ts>
 overload(Ts...) -> overload<Ts...>;
 
-poggles::texture::texture(GLenum target)
-    : m_original_target(target)
+auto poggles::loadFromFile(poggles::texture& tex,
+                           std::filesystem::path const& filename,
+                           std::optional<GLenum> target,
+                           GLint level) -> bool
+{
+  int width = 0;
+  int height = 0;
+  int channels = 0;
+  uint8_t* data = stbi_load(filename.c_str(), &width, &height, &channels, 0);
+  if (data != nullptr) {
+    if (target.has_value()) {
+      tex.target = *target;
+    }
+    // Call ourselves, but with the relevant data extracted from the
+    // file
+    bool status =
+        loadFromData(tex, data, width, height, channels, target, level);
+    stbi_image_free(data);
+    return status;
+  } else {
+    return false;
+  }
+}
+
+auto poggles::loadFromData(poggles::texture& tex,
+                           std::variant<float*, uint8_t*> data,
+                           int width,
+                           int height,
+                           int channels,
+                           std::optional<GLenum> target,
+                           GLint level) -> bool
+{
+  if (target.has_value()) {
+    tex.target = *target;
+  }
+
+  glTexParameteri(tex.target, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(tex.target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(tex.target, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(tex.target, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+  GLenum format = GL_RGB;
+  if (channels == 1) {
+    format = GL_RED;
+  } else if (channels == 2) {
+    format = GL_RG;
+  } else if (channels == 3) {
+    format = GL_RGB;
+  } else if (channels == 4) {
+    format = GL_RGBA;
+  }
+
+  std::visit(overload {[&](float* pixelData)
+                       {
+                         glTexImage2D(tex.target,
+                                      level,
+                                      static_cast<GLint>(format),
+                                      width,
+                                      height,
+                                      0,
+                                      format,
+                                      GL_FLOAT,
+                                      pixelData);
+                       },
+                       [&](uint8_t* pixelData)
+                       {
+                         glTexImage2D(tex.target,
+                                      level,
+                                      static_cast<GLint>(format),
+                                      width,
+                                      height,
+                                      0,
+                                      format,
+                                      GL_UNSIGNED_BYTE,
+                                      pixelData);
+                       }},
+             data);
+  return true;  // TODO: Consider how to handle errors better in poggles - this
+                // should be false on a glError
+}
+
+poggles::texture::texture(GLenum in_target)
+    : target(in_target)
 {
   bind();
 }
 
-void poggles::texture::load(poggles::TextureType type)
+void poggles::texture::bind(GLenum texture_num) const
 {
-  std::visit(
-      overload {
-          [this](TextureFromFile const& textureFromFile)
-          {
-            int width = 0;
-            int height = 0;
-            int channels = 0;
-            uint8_t* data = stbi_load(textureFromFile.filename.c_str(),
-                                      &width,
-                                      &height,
-                                      &channels,
-                                      0);
-            if (data != nullptr) {
-              // Call ourselves, but with the relevant data extracted from the
-              // file
-              TextureFromData textureFromData {
-                  data,
-                  width,
-                  height,
-                  channels,
-                  textureFromFile.level,
-                  textureFromFile.target.value_or(m_original_target)};
-              load(textureFromData);
-            } else {
-              throw std::runtime_error("Failed to load texture");
-            }
-            stbi_image_free(data);
-          },
-          [this](TextureFromData const& textureFromData)
-          {
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  activate(texture_num);
+  glBindTexture(target, static_cast<GLuint>(m_texture_handle.value()));
+}
 
-            GLenum format = GL_RGB;
-            if (textureFromData.channels == 1) {
-              format = GL_RED;
-            } else if (textureFromData.channels == 2) {
-              format = GL_RG;
-            } else if (textureFromData.channels == 3) {
-              format = GL_RGB;
-            } else if (textureFromData.channels == 4) {
-              format = GL_RGBA;
-            }
-            GLenum target = textureFromData.target.value_or(m_original_target);
-
-            std::visit(overload {[&](float* data)
-                                 {
-                                   glTexImage2D(target,
-                                                textureFromData.level,
-                                                static_cast<GLint>(format),
-                                                textureFromData.width,
-                                                textureFromData.height,
-                                                0,
-                                                format,
-                                                GL_FLOAT,
-                                                data);
-                                 },
-                                 [&](uint8_t* data)
-                                 {
-                                   glTexImage2D(target,
-                                                textureFromData.level,
-                                                static_cast<GLint>(format),
-                                                textureFromData.width,
-                                                textureFromData.height,
-                                                0,
-                                                format,
-                                                GL_UNSIGNED_BYTE,
-                                                data);
-                                 }},
-                       textureFromData.data);
-          }},
-      type);
+poggles::texture::operator GLuint() const
+{
+  return static_cast<GLuint>(m_texture_handle.value());
 }
